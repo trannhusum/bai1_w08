6cd21f88a8b2adeb7c8f6a3a6c6b12a755bb06a9
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-07 16:50:36 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1_w08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
